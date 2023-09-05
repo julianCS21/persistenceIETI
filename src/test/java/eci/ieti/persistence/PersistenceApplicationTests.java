d0335f9db70285bf40dc9dfda091f20baabf9545
unknown <jul.1012@hotmail.com>
2023-09-04 22:32:59 -0500

    finished

---
+++ src/test/java/eci/ieti/persistence/PersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.ieti.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
